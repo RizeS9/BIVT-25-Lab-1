875a5dd3e1382e665d2d6a1c51f7a7803ecddc95
RizeS9 <150572730+RizeS9@users.noreply.github.com>
2025-09-09 14:03:29 +0300

    Update Green.cs

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab1
+using System.ComponentModel.Design;
+
+namespace Lab1
 {
     public class Green
     {
@@ -7,60 +9,94 @@
             bool answer = false;
 
             // code here
+            if (Math.Abs(d) >= 1)
+            {
+                answer = true;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task2(double d, double f)
         {
             bool answer = false;
 
             // code here
+            if (((d + f) / 2) > 0)
+            {
+                answer = true;
+            }
+            else
 
-            // end
+                // end
 
-            return answer;
+                return answer;
         }
         public bool Task3(int a, int b)
         {
             bool answer = false;
 
             // code here
+            if ((a + b) > (Math.Abs(a) + Math.Abs(b)) / 2)
+            {
+                answer = true;
+            }
 
-            // end
+                // end
 
-            return answer;
+                return answer;
         }
         public int Task4(int a, int b, int c)
         {
             int answer = 0;
 
             // code here
+            if (a >= b && a >= c)
+            {
+                answer = a;
+            }
+            else if (b >= a && b >= c)
+            {
+                answer = b;
+            }
+            else
+            {
+                answer = c;
+            }
 
-            // end
+                // end
 
-            return answer;
+                return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
 
             // code here
+            if (Math.Abs(x) > 1)
+            {
+                answer = 0;
+            }
+            else
+            {
+                answer = Math.Pow(x, 2) - 1;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task6(double x, double y)
         {
             bool answer = false;
 
             // code here
+            if (y <= 1 + x && y <= 1 - x && y >= 0)
+            {
+                answer = true;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
 
         public bool Task7(int n)
@@ -68,20 +104,38 @@
             bool answer = true;
 
             // code here
+            if (n < 0)
+            {
+                answer = false;
+            }
+            else if (n >= 0)
+            {
+                if( n % 2 == 0)
+                {
+                    answer = false;
+                }
+            }
+             
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task8(int X, int Y)
         {
             bool answer = false;
 
             // code here
+            if 
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
     }
 }
